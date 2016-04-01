69adbbbf383892421c55a1eea2d1fcb2afd538be
lbenitez1 <lbenitez1@gulls.salisbury.edu>
2016-04-01 16:19:48 -0400

    View Edits

--- View.java
+++ View.java
@@ -25,11 +25,11 @@ public class View extends Application {
 	private Scene strategyScene;
 	private VBox mainVBox, strategyVBox, stockVBox;
 	private HBox splitScreen;
-	private Label chooseStrategy, blank1, blank2, title;
+	private Label chooseStrategy, blank1, blank2, blank3, title, decision;
 	private ToggleGroup strategyGroup;
 	private RadioButton strategyRadioOne, strategyRadioTwo, strategyRadioThree, strategyRadioFour;
-	private ListView<String> stockList;
-	private ObservableList<String> stockItems;
+	private ListView<String> stockList, userStockList;
+	private ObservableList<String> stockItems, userStockItems;
 	
 	@Override
 	public void start(Stage primaryStage) {
@@ -62,24 +62,24 @@ public void createAndSetVariables(){
 	chooseStrategy = new Label("Strategy");										//choose strategy Label
 	blank1 = new Label("");														//blank label for spacing
 	blank2 = new Label("");														//blank label for spacing
+	blank3 = new Label("");
 	title = new Label("Title");
+	decision = new Label("Hold");
 	strategyGroup = new ToggleGroup();											//group that holds all the strategy radio buttons
 	strategyGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
 	      public void changed(ObservableValue<? extends Toggle> ov,
 	          Toggle old_toggle, Toggle new_toggle) {
-	        if (strategyGroup.getSelectedToggle() != null) {
-	          System.out.println(strategyGroup.getSelectedToggle().getUserData().toString());
-	        }
+	    	  RadioButton chk = (RadioButton)new_toggle.getToggleGroup().getSelectedToggle(); // Cast object to radio button
+	          System.out.println("Selected Radio Button - "+chk.getText());
 	      }
 	    });
-	strategyRadioOne = new RadioButton("Strategy One");							//first radio button for strategies
-	strategyRadioTwo = new RadioButton("Alternate Strategy");					//second radio button for strategies
-	strategyRadioThree = new RadioButton("Random Strategy");					//third radio button for strategies
-	strategyRadioFour = new RadioButton("Leo and Seans Strategy wink*wink*");	//fourth radio button for strategies
+	addRadioButtons();
 	stockList = new ListView<String>();											//used to hold stock options
 	stockList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 	stockItems = FXCollections.observableArrayList (
 		    "Stock One", "Stock Two", "Stock Three", "Stock Four");				//items to be inserted into list view for stocks
+	userStockList = new ListView<String>();
+	userStockList.setEditable(false);
 	stockList.setItems(stockItems);												//add the items to the list
 	stockList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
 	    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
@@ -88,11 +88,16 @@ public void createAndSetVariables(){
 	    		System.out.println("You have chosen the first stock");
 	    	}
 	    	System.out.println("You chose: "+newValue);
+	    	
+	    	userStockItems = FXCollections.observableArrayList (newValue);				//items to be inserted into list view for stocks
+	    	userStockList.setItems(userStockItems);
 	        
 	    }
 	});
 	stockList.setPrefWidth(500);
 	stockList.setPrefHeight(90);
+	userStockList.setPrefWidth(500);
+	userStockList.setPrefHeight(30);
 }
 public void positionBoxes(){
 	//set alignment
@@ -108,16 +113,20 @@ public void positionBoxes(){
 public void addElementsToScene(){
 	//add strategy radios to strategy group
 	strategyRadioOne.setToggleGroup(strategyGroup);
-	//set this radio to true by default
-	strategyRadioOne.setSelected(true);
 	//continue adding radio buttons
 	strategyRadioTwo.setToggleGroup(strategyGroup);
 	strategyRadioThree.setToggleGroup(strategyGroup);
 	strategyRadioFour.setToggleGroup(strategyGroup);
 	//add label to main panel grid pane first column
-	mainVBox.getChildren().addAll(title, splitScreen);
+	mainVBox.getChildren().addAll(title, splitScreen, decision);
 	strategyVBox.getChildren().addAll(chooseStrategy, blank1, strategyRadioOne, strategyRadioTwo, strategyRadioThree, strategyRadioFour);
-	stockVBox.getChildren().addAll(blank2, stockList);
+	stockVBox.getChildren().addAll(blank2, stockList, blank3, userStockList);
+}
+public void addRadioButtons(){
+	strategyRadioOne = new RadioButton("Strategy One");							//first radio button for strategies
+	strategyRadioTwo = new RadioButton("Alternate Strategy");					//second radio button for strategies
+	strategyRadioThree = new RadioButton("Random Strategy");					//third radio button for strategies
+	strategyRadioFour = new RadioButton("Leo and Seans Strategy wink*wink*");	//fourth radio button for strategies
 }
 }
 
